18ebf28d2d3a0c92bd18e73c9d41133d54528756
rtyner <jtynerjr@my.polk.edu>
2018-03-04 13:16:43 -0500

    initial commit with folder structure

---
+++ Ch_2/languages.py
@@ -0,0 +1,5 @@
+favorite_language = 'python '
+favorite_language = favorite_language.rstrip()
+print("Languages:\n\tPython\n\tC\n\tJavascript")
+print(favorite_language)
+# print(favorite_language.rstrip())
---
+++ Ch_2/name.py
@@ -0,0 +1,11 @@
+name = "ada lovelace"
+first_name = "ada"
+last_name = "lovalace"
+full_name = first_name + " " + last_name
+message = "Hello, " + full_name.title() + "!"
+print(name.title())
+print(name.upper())
+print(name.lower())
+print(full_name.title())
+print("Hello, " + full_name.title() + "!")
+print(message)
