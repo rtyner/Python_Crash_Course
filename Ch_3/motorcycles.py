77fbd17b1dc5005681f6a6d95156ca8fa31fbb56
rtyner <jtynerjr@my.polk.edu>
2018-03-14 08:53:15 -0400

    initial commit

---
+++ Ch_3/motorcycles.py
@@ -0,0 +1,43 @@
+<<<<<<< HEAD
+motorcycles = ['suzuki', 'yamaha', 'honda']
+print(motorcycles)
+motorcycles[0] = 'ducati'
+print(motorcycles)
+motorcycles.append('harley')
+print(motorcycles)
+motorcycles = []
+print(motorcycles)
+motorcycles.append('ducati')
+motorcycles.append('harley')
+motorcycles.append('suzuki')
+motorcycles.append('yamaha')
+print(motorcycles)
+motorcycles = []
+motorcycles.insert(0, 'yamaha')
+motorcycles.insert(1, 'suzuki')
+motorcycles.insert(2, 'ducati')
+motorcycles.insert(3, 'harley')
+print(motorcycles)
+del motorcycles[0]
+print(motorcycles)
+del motorcycles[1]
+print(motorcycles)
+=======
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)
+motorcycles.append('ducati')
+print(motorcycles)
+
+motorcycles = []
+print(motorcycles)
+motorcycles.append('honda')
+motorcycles.append('yamaha')
+motorcycles.append('suzuki')
+print(motorcycles)
+
+motorcycles.insert(0, 'ducati')
+print(motorcycles)
+
+del motorcycles[0]
+print(motorcycles)
+>>>>>>> d445c7f27c7d6a1ad54ffb506aa87417a340f6f7
