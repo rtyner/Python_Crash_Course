33cb295e7d253617365b2420165e31a60a621f4c
rtyner <jtynerjr@my.polk.edu>
2018-03-15 20:47:04 -0400

    initial commit

---
+++ Ch_3/remove_motorcycle.py
@@ -0,0 +1,6 @@
+motorcycles = ['honda', 'suzuki', 'yamaha', 'ducati']
+print(motorcycles)
+too_expensive = 'ducati'
+motorcycles.remove(too_expensive)
+print(motorcycles)
+print("\nA " + too_expensive.title() + " is too expensive for me.")
