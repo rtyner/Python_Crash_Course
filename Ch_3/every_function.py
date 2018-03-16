e38e7df063fe72713bd9e32ddb8a8d4388c2ad8e
rtyner <jtynerjr@my.polk.edu>
2018-03-16 14:50:07 -0400

    initial commi

---
+++ Ch_3/every_function.py
@@ -0,0 +1,15 @@
+# functions to be used:
+# del 
+# sort
+# sorted
+# reverse
+# len
+# title
+# append
+# insert
+# pop
+# remove
+# place in list - ex - var_name[-1]
+distro = ['ubuntu', 'red hat', 'centos', 'debian', 'arch', 
+    'fedora', 'gentoo', 'void', 'kali', 'mint', 'red star']
+print(distro)
---
+++ Ch_3/guest_list_len.py
@@ -0,0 +1,6 @@
+people = ['albert einstein', 'inigo montoya', 'bugs bunny']
+print("Welcome to dinner " + people[0].title())
+print("Welcome to dinner " + people[1].title())
+print("Welcome to dinner " + people[2].title())
+people_len = len(people)
+print("We have " + str(people_len) + " people coming to dinner")
