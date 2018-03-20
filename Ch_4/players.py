9fc77c07cd649055ff2afc4f6e3d72eb8250007b
rtyner <jtynerjr@my.polk.edu>
2018-03-20 12:13:44 -0400

    initial commit

--- Ch_4/players.py
+++ Ch_4/players.py
@@ -3,4 +3,10 @@ print(players[0:3])
 print(players[1:4])
 print(players[:4])
 print(players[:2])
+print(players[2:])
+print(players[:])
+players = ['charles', 'martina', 'michael', 'florence', 'eli']
 
+print("Here are the first three players on my team:")
+for player in players[:3]:
+    print(player.title())
