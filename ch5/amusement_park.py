8beec5047566dc64f4768b4d0ec26f5794f41d3d
rtyner <jtynerjr@my.polk.edu>
2018-04-14 21:59:07 -0400

    continued coursework

--- ch5/amusement_park.py
+++ ch5/amusement_park.py
@@ -1,12 +1,7 @@
-age = 72 
-
+age = 12
 if age < 4:
-    price = 0
+    print("Your admission cost is $0.")
 elif age < 18:
-    price = 5
-elif age < 65:
-    price = 10
-elif age >= 65:
-    price = 5
-
-print("Your admission cost is $" + str(price) + ".")
+    print("Your admission cost is $5.")
+else:
+    print("Your admission cost is $10.")
