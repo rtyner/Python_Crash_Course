3bfc14200b638e5c6dc7a70ac7a27a84f5dac54f
rtyner <jtynerjr@my.polk.edu>
2018-04-14 21:18:06 -0400

    continued work

---
+++ ch5/ord_num.py
@@ -0,0 +1,11 @@
+num = list(range(1,10))
+
+for i in num:
+	if i == '1':
+		print(str(1) + "st")
+	elif i == '2':
+		print(str(2) + "nd")
+	elif i == '3':
+		print(str(3) + "rd")
+	else:
+		print(str(i) + "th")
