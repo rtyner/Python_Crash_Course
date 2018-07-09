9271a4ce79d6477335abfe31e89975baa9b98e13
rtyner <rusty@rtyner.com>
2018-07-09 17:23:56 +0000

    working through course

--- ch6/alien.py
+++ ch6/alien.py
@@ -1,12 +1,5 @@
-alien_0 = {'color': 'green', 'points': 5}
+alien_0 = {'color': 'green', 'points':5}
 
 print(alien_0['color'])
 print(alien_0['points'])
 
-new_points = alien_0['points']
-print("You just earned " + str(new_points) + " points.")
-
-print(alien_0)
-alien_0['x_position'] = 0
-alien_0['y_position'] = 25
-print(alien_0)
