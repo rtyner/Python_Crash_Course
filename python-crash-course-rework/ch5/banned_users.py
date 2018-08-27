0c2dd2e6d19f1306b9e2cafbb551779a72d58ec1
rtyner <rusty@rtyner.com>
2018-08-27 06:06:14 -0400

    continued coursework

---
+++ python-crash-course-rework/ch5/banned_users.py
@@ -0,0 +1,7 @@
+banned_users = ['andrew', 'carolina', 'david']
+user = 'marie'
+
+if user not in banned_users:
+    print("You are not banned")
+else:
+    print("You are banned")
---
+++ python-crash-course-rework/ch5/conditional_tests.py
@@ -0,0 +1,24 @@
+car = 'ford'
+
+print("Is car == 'subaru?")
+print(car == 'subaru')
+
+day = 'monday'
+print(day == 'friday')
+
+month = 'jan'
+print(month == 'feb')
+
+year = '2018'
+print(year == '2018')
+
+keyboard = 'wasd'
+print(keyboard == 'filco')
+
+distro = 'arch'
+print(distro == 'ubuntu')
+
+car = 'bmw'
+print(car == 'bmw'.upper())
+
+print('bmw'.upper())
