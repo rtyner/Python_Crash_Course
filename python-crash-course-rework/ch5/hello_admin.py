7f04ac6f2190c16d6fc1b8615c8e6d97a1d1589b
rt <rusty@rtyner.com>
2018-09-08 23:31:03 -0400

    continued coursework

---
+++ python-crash-course-rework/ch5/hello_admin.py
@@ -0,0 +1,9 @@
+names = ['mike', 'bob', 'carol', 'sarah', 'admin']
+
+for name in names:
+    if name == 'admin':
+        print("Hello admin, would you like a status report?")
+    else:
+        print("Hello " + name + ".")
+
+
