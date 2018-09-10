04ba636f98cb02dc1e7744bbdee4e79351ad217d
rt <rusty@rtyner.com>
2018-09-10 05:36:59 -0400

    continued coursework

--- python-crash-course-rework/ch5/hello_admin.py
+++ python-crash-course-rework/ch5/hello_admin.py
@@ -1,9 +1,10 @@
-names = ['mike', 'bob', 'carol', 'sarah', 'admin']
+# names = ['mike', 'bob', 'carol', 'sarah', 'admin']
+names = []
 
 for name in names:
     if name == 'admin':
         print("Hello admin, would you like a status report?")
     else:
         print("Hello " + name + ".")
-
-
+else:
+    print("No users")
