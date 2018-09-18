0f5a80222d81e5924870600832d659f9666fae47
rt <rusty@rtyner.com>
2018-09-18 05:48:36 -0400

    continued coursework

--- python-crash-course-rework/ch6/aliens.py
+++ python-crash-course-rework/ch6/aliens.py
@@ -1,7 +1,9 @@
+# set key values
 alien_0 = {'color': 'green', 'points': 5}
 alien_1 = {'color': 'yellow', 'points': 10}
 alien_2 = {'color': 'red', 'points': 15}
 
+# empty list to add to later
 aliens = []
 
 # make 30 green aliens
@@ -9,6 +11,7 @@ for alien_number in range(30):
     new_alien = {'color': 'green', 'points': 5, 'speed': 'slow'}
     aliens.append(new_alien)
 
+# manipulate first 3 aliens
 for alien in aliens[0:3]:
     if alien['color'] == 'green':
         alien['color'] = 'yellow'
