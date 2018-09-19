91063e12ce2729f04ab67afeab4aee2993123886
rt <rusty@rtyner.com>
2018-09-19 19:36:57 -0400

    continued coursework

---
+++ python-crash-course-rework/ch6/pets.py
@@ -0,0 +1,24 @@
+pets = []
+
+pet = {
+    'name': 'lars',
+    'animal_type': 'dog',
+    'owner': 'steve',
+}
+pets.append(pet)
+
+pet = {
+    'name': 'petunia',
+    'animal_type': 'cat',
+    'owner': 'ashley',
+}
+pets.append(pet)
+
+pet = {
+    'name': 'doobie',
+    'animal_type': 'bird',
+    'owner': 'dave',
+}
+pets.append(pet)
+
+for pet in pets:
